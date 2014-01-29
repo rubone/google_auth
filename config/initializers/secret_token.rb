ca63319c948467768fc2ad7ee2acc17b83538b7d
rubone <rubone02@gmail.com>
2014-01-29 23:22:29 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoogleAuth::Application.config.secret_key_base = '6d774ee9fb56fff2acafda0ab950903e25636c2991c20a5ccc5c7c7b268ceb87d9b2d84bfb1d664ae96072adb719ce3b1e3bbefbfb9d8cd8bb52de18a6718524'
